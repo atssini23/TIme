8bd9fe4af99c7d820f12ec7d811aae11398f9e43
Atssini Cabrera <atssinicabrera@Atssinis-MacBook-Pro.local>
2018-01-09 17:43:07 -0800

    first

---
+++ Controllers/TimeController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace YourNamespace.Controllers
+{
+    public class TimeController : Controller
+    {
+        [HttpGet]
+        [Route("")]
+        public IActionResult Index()
+        {
+            
+            return View();
+        }
+    }
+}
